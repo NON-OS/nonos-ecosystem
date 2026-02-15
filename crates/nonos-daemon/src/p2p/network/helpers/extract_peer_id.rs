a951f39d177412e81c17ed0dd1228aaf1b27d61a
A. Erik <ekisanon@proton.me>
2026-02-15 17:59:46 +0100

    peer id extraction

---
+++ crates/nonos-daemon/src/p2p/network/helpers/extract_peer_id.rs
@@ -0,0 +1,11 @@
+use libp2p::{Multiaddr, PeerId};
+
+pub fn extract_peer_id(addr: &Multiaddr) -> Option<PeerId> {
+    addr.iter().find_map(|p| {
+        if let libp2p::multiaddr::Protocol::P2p(peer_id) = p {
+            Some(peer_id)
+        } else {
+            None
+        }
+    })
+}
