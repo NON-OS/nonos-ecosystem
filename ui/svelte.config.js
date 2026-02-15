66807b463bd85e101ba6eaba5610207c3be50c14
A. Erik <ekisanon@proton.me>
2026-02-15 18:06:15 +0100

    svelte config

---
+++ ui/svelte.config.js
@@ -0,0 +1,32 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: ''
+		},
+		appDir: '_app',
+		csp: {
+			mode: 'auto',
+			directives: {
+				'default-src': ['self'],
+				'script-src': ['self', 'unsafe-inline'],
+				'style-src': ['self', 'unsafe-inline'],
+				'connect-src': ['self', 'wss:', 'ws:', 'https:'],
+				'img-src': ['self', 'data:', 'blob:']
+			}
+		}
+	}
+};
+
+export default config;
