34607ddeeb2a9126a90b91cfa79b82e10a4f2ae1
A. Erik <ekisanon@proton.me>
2026-02-15 17:59:50 +0100

    duration formatting

---
+++ crates/nonos-daemon/src/p2p/network/helpers/humanize_duration.rs
@@ -0,0 +1,14 @@
+use std::time::Duration;
+
+pub fn humanize_duration(d: Duration) -> String {
+    let secs = d.as_secs();
+    if secs < 60 {
+        format!("{}s", secs)
+    } else if secs < 3600 {
+        format!("{}m", secs / 60)
+    } else if secs < 86400 {
+        format!("{}h", secs / 3600)
+    } else {
+        format!("{}d", secs / 86400)
+    }
+}
