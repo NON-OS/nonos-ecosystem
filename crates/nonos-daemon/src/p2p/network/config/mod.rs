9bb959b5c2b361a6fb649b59fd307da6164c8380
A. Erik <ekisanon@proton.me>
2026-02-15 17:58:06 +0100

    network config module

---
+++ crates/nonos-daemon/src/p2p/network/config/mod.rs
@@ -0,0 +1,9 @@
+mod constants;
+mod network_config;
+mod get_bootstrap_nodes;
+mod get_bootstrap_nodes_for_mode;
+
+pub use constants::*;
+pub use network_config::*;
+pub use get_bootstrap_nodes::*;
+pub use get_bootstrap_nodes_for_mode::*;
