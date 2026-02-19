bb260b03688242e7a205fe04adb1069a5d9739d4
A. Erik <ekisanon@proton.me>
2026-02-19 16:35:57 +0100

    Export blockchain service module

--- crates/nonos-daemon/src/services/mod.rs
+++ crates/nonos-daemon/src/services/mod.rs
@@ -3,12 +3,14 @@ mod health_beacon;
 mod quality_oracle;
 mod bootstrap;
 mod cache;
+mod blockchain;
 
 pub use manager::{ServiceManager, ServiceConfig, ServiceType, ServiceState};
 pub use health_beacon::{HealthBeacon, HealthStatus};
 pub use quality_oracle::QualityOracle;
 pub use bootstrap::{BootstrapService, BootstrapConfig};
 pub use cache::{CacheService, CacheStats};
+pub use blockchain::BlockchainService;
 
 #[cfg(test)]
 mod tests;
